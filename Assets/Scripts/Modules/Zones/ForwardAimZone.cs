0c2388e457da5641ee085f77dc5550ebf1327da0
Laconiq <laconiqofficial@gmail.com>
2026-03-21 13:01:36 +0100

    feat(modules): add ForwardAim zone for FPS weapon aiming
    SphereCast-free zone that creates a virtual aim target along
    Owner.FirePoint.forward. Always returns a target so weapons
    fire every time the trigger activates, like a standard FPS.

---
+++ Assets/Scripts/Modules/Zones/ForwardAimZone.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using AIWE.Interfaces;
+using UnityEngine;
+
+namespace AIWE.Modules.Zones
+{
+    [Serializable]
+    public class ForwardAimZone : ZoneInstance
+    {
+        public override List<ITargetable> SelectTargets(Vector3 origin, float range)
+        {
+            var result = new List<ITargetable>();
+            if (Owner?.FirePoint == null) return result;
+
+            var aimPoint = origin + Owner.FirePoint.forward * range;
+            result.Add(new AimTarget(aimPoint, Owner.FirePoint));
+            return result;
+        }
+
+        public override ZoneInstance CreateInstance()
+        {
+            return new ForwardAimZone();
+        }
+
+        private class AimTarget : ITargetable
+        {
+            public Vector3 Position { get; }
+            public bool IsAlive => true;
+            public Transform Transform { get; }
+
+            public AimTarget(Vector3 position, Transform firePoint)
+            {
+                Position = position;
+                Transform = firePoint;
+            }
+        }
+    }
+}
