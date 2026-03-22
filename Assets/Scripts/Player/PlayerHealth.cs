e22d5262714588d40cbd778466b5e231b4719ae4
Laconiq <laconiqofficial@gmail.com>
2026-03-22 17:41:40 +0100

    feat(player): add death, spectate camera, and respawn system
    - PlayerHealth: Die/Respawn RPCs, visual toggle, SpectateController hookup
    - PlayerController: gate input on IsAlive, subscribe to death/respawn events
    - SpectateController: client-local Cinemachine camera cycling alive players
      via mouse scroll, spectate overlay via GameHUD
    - Dead players respawn automatically at next Preparing phase

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,6 +1,9 @@
 using System;
 using AIWE.Combat;
+using AIWE.Core;
 using AIWE.Interfaces;
+using AIWE.Network;
+using AIWE.Player.CameraEffects;
 using Unity.Netcode;
 using UnityEngine;
 
@@ -19,6 +22,7 @@ namespace AIWE.Player
 
         public event Action<float, float> OnHPChanged;
         public event Action OnDeath;
+        public event Action OnRespawn;
 
         public override void OnNetworkSpawn()
         {
@@ -52,7 +56,65 @@ namespace AIWE.Player
         private void Die()
         {
             OnDeath?.Invoke();
+            DieClientRpc();
+            GameManager.Instance?.CheckTotalPartyKill();
             Debug.Log($"[Player] {gameObject.name} died");
+        }
+
+        [Rpc(SendTo.Everyone)]
+        private void DieClientRpc()
+        {
+            SetVisuals(false);
+
+            if (IsOwner)
+            {
+                var spectate = GetComponent<SpectateController>();
+                if (spectate != null) spectate.EnterSpectate();
+            }
+        }
+
+        private PlayerSpawnManager _spawnManager;
+
+        public void Respawn()
+        {
+            if (!IsServer) return;
+
+            _currentHP.Value = _maxHP;
+
+            if (_spawnManager == null)
+                _spawnManager = FindAnyObjectByType<PlayerSpawnManager>();
+            var spawnPos = _spawnManager != null
+                ? _spawnManager.GetNextSpawnPosition()
+                : Vector3.up;
+
+            var cc = GetComponent<CharacterController>();
+            if (cc != null) cc.enabled = false;
+            transform.position = spawnPos;
+            if (cc != null) cc.enabled = true;
+
+            RespawnClientRpc();
+            OnRespawn?.Invoke();
+        }
+
+        [Rpc(SendTo.Everyone)]
+        private void RespawnClientRpc()
+        {
+            SetVisuals(true);
+
+            if (IsOwner)
+            {
+                var spectate = GetComponent<SpectateController>();
+                if (spectate != null) spectate.ExitSpectate();
+            }
+        }
+
+        private void SetVisuals(bool visible)
+        {
+            var body = transform.Find("PlayerBody");
+            if (body != null) body.gameObject.SetActive(visible);
+
+            var weaponVM = GetComponentInChildren<WeaponViewModel>(true);
+            if (weaponVM != null) weaponVM.gameObject.SetActive(visible);
         }
     }
 }
